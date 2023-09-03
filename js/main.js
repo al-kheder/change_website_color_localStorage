a753665e374dfefe6a218c18329d3f420b852587
Ali <alkhdyyr@gmail.com>
2023-09-03 16:52:27 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+document.body.classList.add(localStorage.getItem('pageColor') || 'red');
+
+var el = document.querySelectorAll('.color-switcher li');
+var classesList = [];
+
+
+ for(var i = 0; i <el.length ; i++){
+    classesList.push(el[i].getAttribute('data-color'));
+
+    el[i].addEventListener('click', function (){
+     document.body.classList.remove(...classesList);
+     document.body.classList.add(this.getAttribute('data-color'))
+
+
+     //add Data to local storage
+     localStorage.setItem('pageColor',this.getAttribute('data-color'))
+    },false)
+  }
